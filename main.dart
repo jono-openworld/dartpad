a9efeb19c0b9cda14713b31cd8b6d9778072a557
jono-openworld <53621272+jono-openworld@users.noreply.github.com>
2020-03-24 07:43:14 -0600

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,63 +1,33 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+void main() {
+  runApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
+      home: ExampleScreen(),
+    ),
+  );
 }
 
-class MyHomePage extends StatelessWidget {
+class ExampleScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter Application Embedded in Website'),
+        title: Text(' Creating a Dartpad with a repository!'),
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'The Gist URL to this website is:',
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              'https://gist.github.com/JoseAlba/a205e21aa0d0a64fe7c977107ce22cbf \n',
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              'The Dartpad URL to this website is:',
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              'https://dartpad.dev/a205e21aa0d0a64fe7c977107ce22cbf \n',
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              'The iFrame code is:',
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              '<iframe src="https://dartpad.dev/embed-flutter.html?id=a205e21aa0d0a64fe7c977107ce22cbf"></iframe> \n',
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              'This Flutter application might be within an iFrame within a Medium post. \n',
-              textAlign: TextAlign.center,
-            ),
+          children: [
+            Text('gh_owner: Owner of the Github Account. In thise case is is: Jose Alba '),
+            Text('gh_repo: Name of the repo within the above account. In this case it is: flutter_code'),
+            Text('gh_path: Path to a dartpad_metadata.yaml file within the repo. In this case it is: lib/dartpad'),
+            Text('Collectivaly to make a dartpad with this repository the URL would be: '),
+            Text('dartpad.dev/embed-flutter.html?gh_owner=JoseAlba&gh_repo=flutter_code&gh_path=lib/dartpad'),
           ],
         ),
       ),
     );
   }
 }
+
